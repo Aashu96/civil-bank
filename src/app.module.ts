b9eb82a4d6e3d656117b5fd4f230d3f4e1f04828
Aashish Chawla <aashishchawla@Aashishs-MacBook-Pro.local>
2022-06-15 11:23:09 +0500

    added changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import  {config}  from './ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JwtModule, JwtService } from '@nestjs/jwt';
@@ -8,9 +7,26 @@ import { User } from './user/user.entity';
 import { UserService } from './user/user.service';
 import { PassportModule, PassportStrategy } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
-
+import { config } from "dotenv";
+config()
 @Module({
-  imports: [TypeOrmModule.forRoot(config), TypeOrmModule.forFeature([User]),JwtModule.register({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    url:process.env.DB_URL,
+    port: 5432,
+    username:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME,
+    entities: [__dirname+'/**/*.entity{.ts,.js}'],
+    synchronize:true,
+    extra:{
+        ssl:{
+            rejectUnauthorized:false
+        }
+    },
+    logging:true,
+  }), TypeOrmModule.forFeature([User]),JwtModule.register({
     secret:'SECRET',
     signOptions:{expiresIn:'60s'}
   }),PassportModule],      
