670fe643c30f4767f6e3ba212f57ee31833d33f8
Aashish Chawla <aashishchawla@Aashishs-MacBook-Pro.local>
2022-06-15 14:05:40 +0500

    first deploy heroku project

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,12 +12,8 @@ config()
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'postgres',
-    host: process.env.DB_HOST,
     url:process.env.DB_URL,
     port: 5432,
-    username:process.env.DB_USER,
-    password:process.env.DB_PASS,
-    database:process.env.DB_NAME,
     entities: [__dirname+'/**/*.entity{.ts,.js}'],
     synchronize:true,
     extra:{
