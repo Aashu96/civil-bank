42fa1a8b8d13ba0490ded1ea35968494a4ae64e7
Aashish Chawla <aashishchawla@Aashishs-MacBook-Pro.local>
2022-06-15 18:18:27 +0500

    fourth  attempt

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap(){
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({
-    credentials:true,
-    origin:'http//:localhost:3000'
+  app.enableCors({credentials:true
   })
   const PORT=process.env.PORT || 3000;
-  console.log(PORT);
-  await app.listen(`PORT NO:${PORT}`);
+  console.log(`Hey ! PORT NO:${PORT}`);
+  await app.listen(PORT);
   
 }
 bootstrap();
